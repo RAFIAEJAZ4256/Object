3272c1b3d3ad94f363d7f3a6fdde2a974e44939e
rafiaejaz <rabiyaejaz133@gmail.com>
2024-10-04 11:42:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var info = {
+    firstName: "Rafia",
+    lastName: "Ejaz",
+    age: 18,
+    qualification: "Intermediate",
+    religion: "Islam"
+};
+document.getElementById("info").innerHTML = info.firstName + "<br>" + info.lastName + "<br>" + info.age + "<br>" + info.qualification + "<br>" + info.religion;
+
+var flv = {
+    flavor1: "Vanilla",
+    flavor2: "Chocolate",
+    flavor3: "Strawberry",
+    flavor4: "Chocolate Chip Cookie Dough"
+
+};
+document.getElementById("flv").innerHTML = flv.flavor1 + "<br>" + flv.flavor2 + "<br>" + flv.flaor3 + "<br>" + flv.flavor4;
+
+var juices = {
+     juice1: "Apple",
+     juice2:  "Blueberry",
+     juice3: "GrapeFruits",
+     juice4: "Orange"
+};
+
+document.getElementById("juices").innerHTML = juices.juice1 + "<br>" + juices.juice2 + "<br>" + juices.juice3 + "<br>" + juices.juice4;
+
+var coffee = {
+    coffee1: "Latte",
+    coffee2: "Cappuccino",
+    coffee3: "Macchiato",
+    coffee4:  "Mocha",
+
+};
+
+document.getElementById("coffee").innerHTML = coffee.coffee1 + "<br>" + coffee.coffee2 + "<br>" + coffee.coffee3 + "<br>" + coffee.coffee4;
+
+  
